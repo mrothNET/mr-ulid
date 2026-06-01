1e81d8925a631fa9a43e3d09224cbbb13b1d94c8
Michael Roth <mail@mroth.net>
2026-06-01 16:16:22 +0200

    Generalize length error messages

--- src/error.rs
+++ src/error.rs
@@ -3,9 +3,9 @@ use std::fmt;
 /// Errors that can occur when creating ULIDs out of foreign data.
 #[derive(Debug, Clone, Copy, Hash, PartialEq, Eq)]
 pub enum Error {
-    /// The ULID string is too short.
+    /// The input is too short.
     TooShort,
-    /// The ULID string is too long.
+    /// The input is too long.
     TooLong,
     /// The ULID string contains an invalid character.
     InvalidChar,
@@ -23,8 +23,8 @@ impl fmt::Display for Error {
     /// Formats the error message for display.
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let message = match *self {
-            Self::TooShort => "string is too short",
-            Self::TooLong => "string is too long",
+            Self::TooShort => "input is too short",
+            Self::TooLong => "input is too long",
             Self::InvalidChar => "string contains an invalid character",
             Self::InvalidZero => "invalid zero value",
             Self::TimestampOutOfRange => "timestamp is too large",
